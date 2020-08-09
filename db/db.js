bbccb610ed07262732e886327ac7e6147bb32d78
brian presta <brian.presta@gmail.comgit commit -m first>
2020-08-09 12:48:22 -0700

    made all queries async

--- db/db.js
+++ db/db.js
@@ -1,14 +1,25 @@
-const connect = require('./credentials')
-const cTable = require('console.table')
+const credentials = require('./credentials')
+const mysql = require("mysql2/promise")
 
 const queryHandler = {
-    showAll(table) {
-        const connection = connect()
-        return connection.query(`select * from ${table}`, (err,result) => {
-            console.table(result)
-            connection.close()
-        })
+    async showAll(table) {
+        const connection = await mysql.createConnection(credentials);
+        const [rows, fields] = await connection.execute(`SELECT * FROM ${table}`);
+        await connection.close();
+        return rows;
+    },
+    async addRecord(table,data) {
+        const connection = await mysql.createConnection(credentials);
+        const [rows, fields] = await connection.execute(`INSERT INTO ${table} SET ?`,data);
+        await connection.close();
+        return rows
+    },
+    async updateRecord(table,data,id) {
+        const connection = await mysql.createConnection(credentials);
+        const [rows, fields] = await connection.execute(`UPDATE ${table} SET ? WHERE id=${id}`,data)
+        await connection.close();
+        return row
     }
 }
 
-queryHandler.showAll('department')
+module.exports = queryHandler
